5cd9052c9378061ba463219a77e47855deef1e05
Omid Shahbazi <sh.omid.m@gmail.com>
2020-08-05 11:18:13 +0430

    Add        Project/BourseAnalyzerServiceCommon/BourseAnalyzerServiceCommon.csproj Add        Project/BourseAnalyzerServiceCommon/Properties/AssemblyInfo.cs Add        Project/BourseAnalyzerServiceCommon/Protocol.cs

---
+++ Project/BourseAnalyzerServiceCommon/Protocol.cs
@@ -0,0 +1,25 @@
+﻿namespace BourseAnalyzerServiceCommon
+{
+	public class LoginReq
+	{
+		public string Username;
+		public string Password;
+	}
+
+	public class LoginRes
+	{
+		public bool Result;
+		public string Message;
+		public int TraderID;
+	}
+
+	public class GetBasicDataReq
+	{
+		public int TraderID;
+	}
+
+	public class GetBasicDataRes
+	{
+		public string[] StocksSymbol;
+	}
+}
